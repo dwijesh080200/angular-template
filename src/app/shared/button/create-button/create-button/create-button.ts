4f55b26324e2ffdc02c642f659b73330529ae12e
dwijesh080200 <dwi.rag123@gmail.com>
2026-04-20 12:59:37 +0400

    added more structural change

---
+++ src/app/shared/button/create-button/create-button/create-button.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-button',
+  imports: [],
+  templateUrl: './create-button.html',
+  styleUrl: './create-button.css',
+})
+export class CreateButton {}
